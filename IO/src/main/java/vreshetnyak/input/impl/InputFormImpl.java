416229c532c3e695b78aa114390307c42e6280a1
mirabon <vasilij.reshetnyak@gmail.com>
2014-12-03 23:36:29 +0200

    add menu

--- IO/src/main/java/vreshetnyak/input/impl/InputFormImpl.java
+++ IO/src/main/java/vreshetnyak/input/impl/InputFormImpl.java
@@ -6,6 +6,7 @@ package vreshetnyak.input.impl;
 
 import java.awt.ComponentOrientation;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import javax.swing.*;
 import vreshetnyak.input.AbstractInput;
 
@@ -15,7 +16,7 @@ import vreshetnyak.input.AbstractInput;
  */
 public class InputFormImpl extends AbstractInput{
 
-    private String[] CommandInput = {"Translate", "InMemory", "Statistic", "Analytics"};
+    //private String[] CommandInput = {"Translate", "InMemory", "Statistic", "Analytics"};
     protected JButton SendDataToSensoButton;
     protected JTextArea DataJTextArea;
     protected JComboBox CommandJComboBox;
@@ -45,18 +46,38 @@ public class InputFormImpl extends AbstractInput{
         InputFormFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         InputFormFrame.setLocationRelativeTo(null);
         InputFormFrame.setResizable(false);
-        InputFormFrame.setLayout(new FlowLayout(FlowLayout.CENTER));
+        InputFormFrame.setLayout(new GridLayout(2, 2));
         
 
+        /**
+         * Menu
+         */
+        
+        JMenuBar BarMenu = new JMenuBar();
+        JMenu menu = new JMenu("Команда");
+
+        JMenuItem MItemTranstate = new JMenuItem("Translate");
+        JMenuItem MItemInMem = new JMenuItem("InMemory");
+        JMenuItem MItemStat = new JMenuItem("Statistic");
+        JMenuItem MItemAnalitic = new JMenuItem("Analytics");
+
+        menu.add(MItemTranstate);
+        menu.add(MItemInMem);
+        menu.add(MItemStat);
+        menu.add(MItemAnalitic);
+
+        BarMenu.add(menu);
+        InputFormFrame.add(BarMenu);
+        
         /**
          * Добавляем выпадающий список
          */
         
-        CommandJComboBox = new JComboBox(CommandInput);
-        JPanel CommandJPanel = new JPanel();
-        CommandJPanel.add(CommandJComboBox);
-        CommandJPanel.setComponentOrientation(ComponentOrientation.UNKNOWN);
-        InputFormFrame.add(CommandJPanel);
+//        CommandJComboBox = new JComboBox(CommandInput);
+//        JPanel CommandJPanel = new JPanel();
+//        CommandJPanel.add(CommandJComboBox);
+//        CommandJPanel.setComponentOrientation(ComponentOrientation.UNKNOWN);
+//        InputFormFrame.add(CommandJPanel);
         
         /**
          * Добавляем JTextArea для многострочных текстов со скролом
