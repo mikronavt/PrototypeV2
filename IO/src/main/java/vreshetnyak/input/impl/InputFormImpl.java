b241eb52fc46f207f05ba4ed713fff5a71240074
mirabon <vasilij.reshetnyak@gmail.com>
2014-12-03 22:03:17 +0200

    Залил форму ввода

---
+++ IO/src/main/java/vreshetnyak/input/impl/InputFormImpl.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vreshetnyak.input.impl;
+
+import java.awt.ComponentOrientation;
+import java.awt.FlowLayout;
+import javax.swing.*;
+import vreshetnyak.input.AbstractInput;
+
+/**
+ *
+ * @author Василий
+ */
+public class InputFormImpl extends AbstractInput{
+
+    private String[] CommandInput = {"Translate", "InMemory", "Statistic", "Analytics"};
+    protected JButton SendDataToSensoButton;
+    protected JTextArea DataJTextArea;
+    protected JComboBox CommandJComboBox;
+
+    /**
+     * Унаследованный метод образует форму для ввода команд данных.
+     * Команды выбираются из выпадающего списка CommandJComboBox, которые
+     * принимает массив команд переменной CommandInput.
+     * 
+     * На кнопку SendDataToSensoButton повешен слушатель SendDataToSensor()
+     * Класс которого описан здесь же. Наследует ActionListener
+     * 
+     * Выполняет единственный (пока!) метод actionPerformed, который
+     * формирует два поля - (String)title и command. 
+     * 
+     * Метод вызывает метод AcceptTranslateForOutput(title, command) 
+     * из объекта класса com.​artech.​prototype.​output.InitOutputForm
+     */
+    @Override
+    public void InputGUIForm() {
+        /**
+         * Начинаем строить форму
+         */
+        
+        JFrame InputFormFrame = new JFrame("Ввод данных для обработки");
+        InputFormFrame.setSize(400, 500);
+        InputFormFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        InputFormFrame.setLocationRelativeTo(null);
+        InputFormFrame.setResizable(false);
+        InputFormFrame.setLayout(new FlowLayout(FlowLayout.CENTER));
+        
+
+        /**
+         * Добавляем выпадающий список
+         */
+        
+        CommandJComboBox = new JComboBox(CommandInput);
+        JPanel CommandJPanel = new JPanel();
+        CommandJPanel.add(CommandJComboBox);
+        CommandJPanel.setComponentOrientation(ComponentOrientation.UNKNOWN);
+        InputFormFrame.add(CommandJPanel);
+        
+        /**
+         * Добавляем JTextArea для многострочных текстов со скролом
+         */
+
+        DataJTextArea = new JTextArea(20, 27);
+        DataJTextArea.setLineWrap(true);
+        DataJTextArea.setWrapStyleWord(true);
+
+        JScrollPane scrollPane = new JScrollPane(DataJTextArea);
+        scrollPane.setViewportView(DataJTextArea);
+
+        InputFormFrame.add(scrollPane);
+        InputFormFrame.setVisible(true);
+        
+    }
+    
+}
