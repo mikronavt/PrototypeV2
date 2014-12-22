c98af323f714a20c1085fe520bb14e278ee2f19c
Tsaplin Anatoliy <chgb-tol@ya.ru>
2014-12-22 10:34:10 +0300

    поправил скрипты для БД

--- Saver/src/main/resources/mysql/Create_Idf_en_MySql.sql
+++ Saver/src/main/resources/mysql/Create_Idf_en_MySql.sql
@@ -1,7 +1,7 @@
-/*Создание таблицы IDF для англоязычных слов*/
+﻿/*Создание таблицы IDF для англоязычных слов*/
 CREATE TABLE IF NOT EXISTS IDF_EN(
     ENID INT(20) NOT NULL,
     IDF FLOAT(20) NOT NULL,
-    PRIMARY_KEY (ENID),
+    PRIMARY KEY (ENID),
     CONSTRAINT FOREIGN KEY (ENID) REFERENCES DICTIONARY_EN (ENID) ON DELETE CASCADE ON UPDATE CASCADE
 );
--- Saver/src/main/resources/mysql/Create_Idf_ru_MySql.sql
+++ Saver/src/main/resources/mysql/Create_Idf_ru_MySql.sql
@@ -1,7 +1,7 @@
-/*Создание таблицы IDF для русскоязычных слов*/
+﻿/*Создание таблицы IDF для русскоязычных слов*/
 CREATE TABLE IF NOT EXISTS IDF_RU(
     RUID INT(20) NOT NULL,
     IDF FLOAT(20) NOT NULL,
-    PRIMARY_KEY (RUID),
+    PRIMARY KEY (RUID),
     CONSTRAINT FOREIGN KEY (RUID) REFERENCES DICTIONARY_RU (RUID) ON DELETE CASCADE ON UPDATE CASCADE
 );
